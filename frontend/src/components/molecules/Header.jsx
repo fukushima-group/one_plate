1ff998ff74da9df7faf882f6c0fbb7bfbd8ec518
junichi-fukushima <71912259+junichi-fukushima@users.noreply.github.com>
2021-06-01 21:07:59 +0900

    ・ハンバーガーメニュー＆ログインボタンの設置
    だいぶずれているけど
    ・What
    ハンバーガーメニュの実装とリンクの設置
    ログインボタンの設置

--- frontend/src/components/molecules/Header.jsx
+++ frontend/src/components/molecules/Header.jsx
@@ -1,59 +1,38 @@
 import React , { Component, useState } from 'react';
-import styled from 'styled-components';
+import './style.scss'
 import { slide as Menu } from "react-burger-menu";
 
-// header
-const GlobalHeader = styled.header`
-  display:flex;
-  position:fixed;
-  width: 100%;
-  height:72px;
-  background-color: white;
-  justify-content: space-between;
-  align-items: center;
-  padding:0 15px;
-  z-index:36500;
-`;
 
-const HeaderTitle = styled.h2`
-`;
-
-const Link = styled.a`
-
-`;
-
-var styles = {
-  bmMenuWrap: {
-    transition: ''
-  }
-}
 
 export const Header = ({
   ...props
-  
 }) => {
   return (
-    <GlobalHeader>
-      <HeaderTitle>One Plate</HeaderTitle>
-      <Menu {...props} left width={250} 
-      pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
-        <Link to="/some-page-link" className="menu-item" >
-          ホームページ
-        </Link>
-
-        <Link to="/some-page-link" className="menu-item" >
-          ページ2
-        </Link>
-
-        <Link to="/some-page-link" className="menu-item" >
-        ページ3
-        </Link>
-
-        <Link to="/some-page-link" className="menu-item" >
-        ページ4
-        </Link>
-       </Menu>
-    </GlobalHeader>
+    <div>
+      <div className="headerLeft">
+        <h2>OnePlate</h2>
+      </div>
+      <div>
+        <Menu {...props} left
+          pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } >
+              <a to="/some-page-link" className="menu-item" >
+                投稿一覧ページ
+              </a>
+              <a to="/some-page-link" className="menu-item" >
+                ページ2
+              </a>
+              <a to="/some-page-link" className="menu-item" >
+                ページ3
+              </a>
+              <a to="/some-page-link" className="menu-item" >
+                ページ4
+              </a>
+        </Menu>
+      </div>
+      <div className="headerRight">
+        <p><a>ログイン</a></p>
+      </div>
+    </div>
     );
 };
 
